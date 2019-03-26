a56a7272669cbeb7017465e80e2392a3938e3058
Djane Rey Mabelin <d.mabelin@arcanys.com>
2019-03-26 14:45:01 +0800

    add fields

--- src/models/bills.model.js
+++ src/models/bills.model.js
@@ -6,7 +6,8 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const bills = new Schema({
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    description: { type: String, required: true }
   }, {
     timestamps: true
   });
--- test/services/bills.test.js
+++ test/services/bills.test.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 
 const memory = require('feathers-memory');
 const hooks = require('../../src/services/bills/bills.hooks');
+const createModel = require('../../src/models/bills.model');
 
 
 
@@ -21,6 +22,7 @@ describe('\'Bills\' service', () => {
     }));
 
     app.service('bills').hooks(hooks);
+    app.set('mongooseClient', require('mongoose'));
   });
 
   it('registered the service', () => {
@@ -50,6 +52,22 @@ describe('\'Bills\' service', () => {
       }).then((results) => {
         assert.equal(results.data.length, 1);
       }).then(done).catch(done);
+  });
+
+  it('accept name, description only', (done) => {
+    const Model = createModel(app);
+    const validModel = new Model({name: 'Anti-smoking bill', description: 'Nobody is allowed to smoke'});
+    validModel.validate();
+    assert.equal(validModel.name, 'Anti-smoking bill');
+    assert.equal(validModel.description, 'Nobody is allowed to smoke');
+
+    const modelWithExtraFields = new Model({name: 'Anti-smoking bill', description: 'Nobody is allowed to smoke', nothing: 'This is extra'});
+    modelWithExtraFields.validate();
+    assert.equal(modelWithExtraFields.name, 'Anti-smoking bill');
+    assert.equal(modelWithExtraFields.description, 'Nobody is allowed to smoke');
+    assert.equal(modelWithExtraFields.nothing, undefined);
+
+    done();
   });
   
 });
